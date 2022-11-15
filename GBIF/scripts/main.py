d10a8a432a6c79fc043b00079f1297f9070e4cd3
Colin Smith <colin.smith@wisc.edu>
2022-11-15 13:01:05 -0800

    Do not constrain format of L1 ID
    Constraining the ID to a specific format prevents the resuse of this code by other data repositories and only provides minor benefits here

--- GBIF/scripts/main.py
+++ GBIF/scripts/main.py
@@ -11,7 +11,6 @@ fname = "processing_log.csv"
 print("Processing log integrity checker")
 check.add_level2(level2=L2_scope_acc_rev, filename=fname)
 check.find_duplicates(filename=fname)
-# TODO check package ID is in scope.accession.revision format
 
 # Register the incoming L2_scope_acc_rev
 gbif_id = register(level2=L2_scope_acc_rev, filename=fname)
