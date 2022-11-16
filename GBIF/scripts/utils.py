9ae2ab61459aa67a116cb2cf6ceb88e5f53435d4
Colin Smith <colin.smith@wisc.edu>
2022-11-16 15:18:26 -0800

    Dev read_log() and tests
    Let pandas enforce the column name and type constraints rather than doing this ourselves.

--- GBIF/scripts/utils.py
+++ GBIF/scripts/utils.py
@@ -4,11 +4,25 @@ import pandas as pd
 def read_log(f):
     """
     Read log file to Pandas Dataframe
-    :param f: Path to log file
-    :return: Pandas Dataframe
+
+    Parameters
+    ----------
+    f
+        Path to the log file.
+
+    Returns
+    -------
+    Dataframe
+
+    See Also
+    --------
+    init_log : Initialize a log file.
     """
-    log = pd.read_csv(f, delimiter=',')
+    required_cols = ['id_local', 'id_gbif', 'crawled']
+    log = pd.read_csv(f, delimiter=',', usecols=required_cols)
+    log['crawled'] = pd.to_datetime(log['crawled'])
     return log
+
 
 def write_log(f):
     pass
--- GBIF/tests/test_utils.py
+++ GBIF/tests/test_utils.py
@@ -5,3 +5,5 @@ from GBIF.scripts import utils
 def test_read_log():
     log = utils.read_log('../data/processing_log.csv')
     assert isinstance(log, pd.DataFrame)
+    assert pd.core.dtypes.common.is_datetime64_dtype(log['crawled'])
+
