fbf5f4eb92d296de2244c25c9680a795f69622ad
Colin Smith <colin.smith@wisc.edu>
2022-11-17 09:32:52 -0800

    Test for expected cols

--- GBIF/tests/test_utils.py
+++ GBIF/tests/test_utils.py
@@ -4,6 +4,8 @@ from GBIF.scripts import utils
 
 def test_read_log():
     log = utils.read_log('../data/processing_log.csv')
+    expected_cols = {'id_gbif', 'id_local', 'crawled'}
     assert isinstance(log, pd.DataFrame)
+    expected_cols.issubset(set(log.columns))
     assert pd.core.dtypes.common.is_datetime64_dtype(log['crawled'])
 
