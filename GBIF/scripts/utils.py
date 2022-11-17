2c342b2dfa4a019ed8ca889c6a14bbbc5e3d94e5
Colin Smith <colin.smith@wisc.edu>
2022-11-17 09:32:15 -0800

    Note parameter expectations

--- GBIF/scripts/utils.py
+++ GBIF/scripts/utils.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 
+def init_log(f):
+    pass
+
+
 def read_log(f):
     """
     Read log file to Pandas Dataframe
@@ -8,15 +12,20 @@ def read_log(f):
     Parameters
     ----------
     f
-        Path to the log file.
+        Log file path.
 
     Returns
     -------
     Dataframe
 
+    Notes
+    -----
+    The log file is a .csv with the minimum column set: 'id_local', 'id_gbif', and 'crawled', where 'crawled' is a
+    datetime of a format parsable by pandas `read_csv()`.
+
     See Also
     --------
-    init_log : Initialize a log file.
+    init_log : Initialize log file.
     """
     required_cols = ['id_local', 'id_gbif', 'crawled']
     log = pd.read_csv(f, delimiter=',', usecols=required_cols)
