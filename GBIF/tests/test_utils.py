8b345809cc619c1e4e12f2e9203909238443b747
Colin Smith <colin.smith@wisc.edu>
2022-11-18 10:06:18 -0800

    Add test fixture for log file
    Trying to be DRY

--- GBIF/tests/test_utils.py
+++ GBIF/tests/test_utils.py
@@ -4,14 +4,18 @@ from GBIF.scripts import utils
 import pytest
 
 
+@pytest.fixture
+def log():
+    return pd.read_csv('../data/processing_log.csv', delimiter=',')
+
+
 def test_read_log():
     log = utils.read_log('../data/processing_log.csv')
     assert isinstance(log, pd.DataFrame)
     assert pd.core.dtypes.common.is_datetime64_dtype(log['crawled'])
 
 
-def test_validate_log():
-    log = pd.read_csv('../data/processing_log.csv', delimiter=',')
+def test_validate_log(log):
     bad_log = log[['id_local', 'id_gbif']]  # Log missing a required column
     with pytest.raises(ValueError):
         utils.validate_log(bad_log)
